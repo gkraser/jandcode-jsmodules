1d7a292e1f36b161c3af43ddf55ece2701d0f272
gkraser <gkraser@gmail.com>
2021-10-12 14:08:16 +0600

    ~~

--- js-modules/jandcode-tools/src/index.js
+++ js-modules/jandcode-tools/src/index.js
@@ -1,10 +1,14 @@
 //
 let jcModules = require("./jc-modules")
 let webpackBaseConfig = require("./webpack-base-config")
-let jcVueLoader = require.resolve('./vue-loader')
+let jcVueLoader = require.resolve('./vue-loader.')
+let fileUtils = require.resolve('./file-utils')
+let moduleUtils = require.resolve('./module-utils')
 
 module.exports = {
     jcModules,
     webpackBaseConfig,
     jcVueLoader,
+    ...fileUtils,
+    ...moduleUtils,
 }
