f36f415be6773e584429dab463079a4f92d044b6
gkraser <gkraser@gmail.com>
2021-10-20 10:39:51 +0600

    ~~

---
+++ js-modules/jandcode-apx-ui/test/tests/dummy.test.js
@@ -0,0 +1,9 @@
+//import {tst} from './vendor'
+
+describe(module.id, function() {
+
+    it("dummy1", function() {
+    })
+
+})
+    
---
+++ js-modules/jandcode-apx-ui/test/vendor.js
@@ -0,0 +1,11 @@
+//
+import * as tst from '@jandcode/tst'
+//
+import * as jcBase from '@jandcode/base'
+import * as apx from '@jandcode/apx'
+//
+export {
+    tst,
+    jcBase,
+    apx,
+}
