4c65a217402fd00e9147f466c27f9f8dc40eaaee
gkraser <gkraser@gmail.com>
2021-10-25 07:50:53 +0600

    ~~

--- js-modules/jandcode-apx/src/quasar/index.js
+++ js-modules/jandcode-apx/src/quasar/index.js
@@ -1,2 +1,24 @@
-//
-import './init'
+import {Quasar} from '../vendor'
+import {initVueApp} from '../vue'
+import {quasarIconSet} from '../icons/quasar-iconSet'
+import {quasar_iconMapFn} from '../utils/icons'
+
+/**
+ * Плагины quasar, которые включаются
+ */
+export let quasarPlugins = [
+    Quasar.Dialog,
+    Quasar.Notify,
+    Quasar.Loading,
+]
+
+initVueApp((vueApp) => {
+    vueApp.use(Quasar.Quasar, {
+        plugins: quasarPlugins,
+        iconSet: quasarIconSet,
+    })
+    vueApp.config.globalProperties.$q.iconMapFn = (iconName) => {
+        return quasar_iconMapFn(iconName)
+    }
+})
+
