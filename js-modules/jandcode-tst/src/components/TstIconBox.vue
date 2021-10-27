7bb1dfa6f0eb8af96822a939393ed298fecffddf
gkraser <gkraser@gmail.com>
2021-10-27 11:39:07 +0600

    ~~

---
+++ js-modules/jandcode-tst/src/components/TstIconBox.vue
@@ -0,0 +1,121 @@
+<template>
+    <div class="tst-icon-box column inline items-center q-pa-md" :data-icon="iconName">
+
+        <q-badge transparent class="tst-icon-box--icon-type self-end"
+                 :label="iconType"
+                 :text-color="iconTypeColor"/>
+
+        <q-tooltip>
+            <span class="text-yellow">{{ iconName }}</span>
+            <span class="text-white"> : {{ iconValue }}</span>
+        </q-tooltip>
+
+        <q-icon :name="iconName" class="col q-mb-md tst-icon-box--icon"
+                :style="{fontSize: iconSize, color:iconColor}"
+                :class="{'tst-icon-box--icon-border':iconBorder}"
+        />
+
+        <div class="tst-icon-box--icon-name col text-size-sm text-gray-700">
+            {{ iconName }}
+        </div>
+
+    </div>
+</template>
+
+<script>
+import {apx} from '../vendor'
+
+export let colors = {
+    'font': 'red',
+    'svg': 'accent',
+    'img:svg': 'green',
+    'img:png': 'blue',
+    'other': 'orange'
+}
+
+export default {
+    name: 'tst-icon-box',
+    props: {
+        icon: {
+            default: 'bus'
+        },
+        iconSize: {
+            default: '2rem'
+        },
+        iconColor: {
+            default: '#616161'
+        },
+        iconBorder: {
+            default: false
+        }
+    },
+    data() {
+        return {}
+    },
+    computed: {
+        iconName() {
+            return apx.icons.getIcon(this.icon) ? this.icon : 'empty'
+        },
+        iconValue() {
+            return apx.icons.getIcon(this.icon) || 'empty'
+        },
+        iconType() {
+            let v = this.iconValue
+            let type = 'font'
+            if (v.startsWith('img:')) {
+                type = 'img'
+                let a = v.lastIndexOf('.')
+                if (a !== -1) {
+                    type = type + ':' + v.substring(a + 1)
+                }
+            } else if (v.startsWith('svg:')) {
+                type = 'svg'
+            }
+            return type
+        },
+        iconTypeColor() {
+            return colors[this.iconType] || colors['other']
+        }
+    },
+}
+</script>
+
+<style lang="less">
+
+.tst-icon-box {
+    width: 12rem;
+
+    &--icon-name {
+        max-width: 10rem;
+        text-overflow: ellipsis;
+        overflow: hidden;
+        white-space: nowrap;
+    }
+
+    &--icon-type {
+        position: relative;
+        right: -0.8rem;
+        top: -0.8rem;
+        background: transparent;
+    }
+
+    &--icon {
+        border: 1px solid transparent;
+    }
+
+    &--icon-border {
+        border: 1px solid gray;
+    }
+
+    // это должно быть в теме, без нее иконки не квадратные!
+    .q-icon {
+        svg {
+            width: 1em;
+            height: 1em;
+            fill: currentColor;
+        }
+    }
+
+}
+
+</style>
--- js-modules/jandcode-tst/src/components/index.js
+++ js-modules/jandcode-tst/src/components/index.js
@@ -3,6 +3,8 @@ import TstCheckbox from './TstCheckbox'
 import TstFontsize from './TstFontsize'
 import TstSelect from './TstSelect'
 import TstPanel from './TstPanel'
+import TstIconBox from './TstIconBox'
+import TstIconList from './TstIconList'
 
 export {
     TstBtn,
@@ -10,6 +12,8 @@ export {
     TstFontsize,
     TstSelect,
     TstPanel,
+    TstIconBox,
+    TstIconList,
 }
 
 
