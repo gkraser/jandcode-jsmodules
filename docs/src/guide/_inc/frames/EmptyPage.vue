4f6b96f188d674a9fa92ffcf1ed3bdc611e8043a
gkraser <gkraser@gmail.com>
2021-10-30 19:44:26 +0600

    ~~

---
+++ docs/src/guide/_inc/frames/EmptyPage.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        Страница
+    </div>
+</template>
+
+<script>
+import {apx} from './vendor'
+export default {
+    extends: apx.JcFrame,
+}
+</script>
---
+++ docs/src/guide/_inc/frames/SimplePage.vue
@@ -0,0 +1,21 @@
+<template>
+    <decor-page>
+        <template #toolbar>
+            <jc-action icon="mail"/>
+            <jc-action icon="calendar"/>
+        </template>
+        Страница
+    </decor-page>
+</template>
+
+<script>
+import {apx} from './vendor'
+export default {
+    extends: apx.JcFrame,
+    created() {
+        this.title = 'Простая страница'
+        this.title2 = 'Есть мелкий заголовок'
+        this.icon = 'frame'
+    },
+}
+</script>
