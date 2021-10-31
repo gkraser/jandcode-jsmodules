3fedf428ac9227142d4da59c55ac9ff08ea9af5a
gkraser <gkraser@gmail.com>
2021-10-31 07:26:34 +0600

    ~~

---
+++ js-modules/jandcode-apx-ui/test/pages/_components/ClassExam.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="row items-center q-mb-sm q-col-gutter-sm">
+        <div :class="cls" class="col-10">
+            <slot></slot>
+        </div>
+        <div class="col-2">
+            <q-badge align="middle" outline1 color="primary" :label="cls"/>
+        </div>
+    </div>
+</template>
+
+<script>
+/* Демонстрация класса для типографики
+----------------------------------------------------------------------------- */
+
+export default {
+    props: {
+        cls: {
+            default: 'text'
+        }
+    },
+    data() {
+        return {}
+    },
+    methods: {},
+}
+</script>
