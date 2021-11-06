f64bc5648ea8c814ef5372753739b9515a7860a7
gkraser <gkraser@gmail.com>
2021-11-06 15:49:16 +0600

    ~~

---
+++ js-modules/jandcode-apx-ui/test/pages/Content1.test.vue
@@ -0,0 +1,33 @@
+<template>
+    <tst-panel fontsize>
+        <div v-html="htmlText" class="content"/>
+    </tst-panel>
+</template>
+
+<script>
+import '../vendor'
+
+import html1 from './_data/page1.html'
+
+console.info("html1",html1);
+
+export default {
+    created() {
+        this.htmlText = html1
+    },
+    data() {
+        return {}
+    },
+    methods: {}
+}
+</script>
+
+<style lang="less">
+
+.content {
+    width: 50vw;
+    font-size: 15px;
+    line-height: 1.4;
+}
+
+</style>
