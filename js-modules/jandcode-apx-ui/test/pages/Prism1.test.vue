a8fed4589af18cc3545d5848d1bb42d56a195fb4
gkraser <gkraser@gmail.com>
2021-11-07 08:54:56 +0600

    test

---
+++ js-modules/jandcode-apx-ui/test/pages/Prism1.test.vue
@@ -0,0 +1,28 @@
+<template>
+    <tst-panel>
+        <div class="q-gutter-md">
+            <jc-btn label="Prism highlight" @click="highlight"/>
+            <div>
+                <pre><code class="lang-javascript">s="ssss"</code></pre>
+            </div>
+        </div>
+    </tst-panel>
+</template>
+
+<script>
+import {apx} from '../vendor'
+import Prism from 'prismjs';
+import prismCss from 'prismjs/themes/prism.css'
+
+Prism.manual = true
+
+apx.jcBase.applyCss(prismCss)
+
+export default {
+    methods: {
+        highlight() {
+            Prism.highlightAllUnder(this.$el)
+        }
+    }
+}
+</script>
