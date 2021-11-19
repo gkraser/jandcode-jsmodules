b507425b210b131717e0b821298ab5ffd088e3a5
gkraser <gkraser@gmail.com>
2021-11-19 11:01:58 +0600

    dom: pixel calc

--- js-modules/jandcode-base/src/dom.js
+++ js-modules/jandcode-base/src/dom.js
@@ -203,6 +203,231 @@ export function resizeWatch(el, trigger, debonceTimeout) {
 //////
 
 /**
+ * Калькулятор размеров px.
+ * Позволяет переводить размеры css в px.
+ *
+ * Принцип работы.
+ *
+ * Формируется иерархия элементов из options.parentElements. К ним добавляется еще
+ * один, самого нижнего уровня. При расчете размера самому нижнему элементу ставятся
+ * соотвествующие стили. Фактически размер возвращается из элемента, на который
+ * указывает options.targetElement.
+ *
+ * Особые искуственные еденицы измерения:
+ * * 'NNchar' - для getWidth, ширина, если содержимое NN-нулей, например '20char'
+ * * 'NNline' - для getHeight, высота, если содержимое NN-строк, например '2line'
+ */
+export class PixelCalc {
+
+    /**
+     * @param {Object} options опции
+     * @param {Array} options.classList список классов элемента, для которого расчитывается
+     * размер
+     * @param {Array} options.parentElements списко элементов, которые станут родителями
+     * ноды, в которой будут расчитываться размеры. Может быть Element (если он первый -
+     * воссоздается его иерархия до body), строка (класс), массив строк (массив классов)
+     * @param {String} options.targetElement селектор для элемента, чьи размеры будут
+     * возвращатся. Если не указано, то принимается по умолчанию
+     * @param {Boolean} options.round округлять ли до int, по умолчанию - да
+     */
+    constructor(options) {
+        this.options = Object.assign({}, options)
+        this.tmpEl = createTmpElement()
+
+        let elements = this.__makeEl(this.options.parentElements)
+        this.el = elements.el
+        this.tmpEl.appendChild(elements.top)
+
+        this.targetEl = this.el
+        if (this.options.targetElement) {
+            let targetEl = this.tmpEl.querySelector(this.options.targetElement)
+            if (targetEl) {
+                this.targetEl = targetEl
+            }
+        }
+
+        if (cnv.isArray(this.options.classList)) {
+            this.el.classList.add(...this.options.classList)
+        }
+        this.round = true
+        if ('round' in this.options) {
+            this.round = !!this.options.round
+        }
+        this.cacheWidth = {}
+        this.cacheHeight = {}
+
+    }
+
+    destroy() {
+        if (this.tmpEl) {
+            this.tmpEl.remove()
+            this.tmpEl = null
+        }
+    }
+
+    /**
+     * Для ширины в css возвращает ширину в px
+     * @param size ширина в css (em, rem...)
+     * @return {number}
+     */
+    getWidth(size) {
+        if (cnv.isNumber(size)) {
+            if (this.round) {
+                return Math.trunc(size)
+            }
+            return size
+        }
+        let z = this.cacheWidth[size]
+        if (z != null) {
+            return z
+        }
+        this.el.innerHTML = ''
+        if (size.endsWith('char')) {
+            this.el.style.width = 'auto'
+            let cnt = cnv.toInt(size.substring(0, size.length - 4))
+            this.el.innerHTML = "0".repeat(cnt)
+        } else {
+            this.el.style.width = size
+        }
+        this.el.style.height = '1em'
+        let cst = window.getComputedStyle(this.el)
+
+        let bcr = this.targetEl.getBoundingClientRect()
+        z = bcr.width
+        if (this.round) {
+            z = Math.trunc(z)
+            if (z === 0) {
+                z = 1
+            }
+        }
+        this.cacheWidth[size] = z
+
+        return z
+    }
+
+    /**
+     * Для ширины в css возвращает ширину в px
+     * @param size ширина в css (em, rem...)
+     * @return {number}
+     */
+    getHeight(size) {
+        if (cnv.isNumber(size)) {
+            if (this.round) {
+                return Math.trunc(size)
+            }
+            return size
+        }
+        let z = this.cacheHeight[size]
+        if (z != null) {
+            return z
+        }
+        this.el.innerHTML = ''
+        if (size.endsWith('line')) {
+            this.el.style.height = 'auto'
+            let cnt = cnv.toInt(size.substring(0, size.length - 4))
+            this.el.innerHTML = "0<br>".repeat(cnt)
+        } else {
+            this.el.style.height = size
+        }
+        this.el.style.width = '1em'
+
+        let bcr = this.targetEl.getBoundingClientRect()
+        z = bcr.height
+        if (this.round) {
+            z = Math.trunc(z)
+            if (z === 0) {
+                z = 1
+            }
+        }
+        this.cacheHeight[size] = z
+        return z
+    }
+
+    //////
+
+    __makeEl(parentElelemns) {
+        let nodes = []
+
+        let cloneNode = (node) => {
+            let n = node.cloneNode(false)
+
+            n.style.width = 'auto'
+            n.style.minWidth = 'auto'
+            n.style.maxWidth = 'auto'
+            n.style.height = 'auto'
+            n.style.minHeight = 'auto'
+            n.style.maxHeight = 'auto'
+            n.style.position = 'static'
+
+            return n
+        }
+
+        let cloneNodeUp = (node) => {
+            let res = []
+            let cur = node
+            while (cur) {
+                if (cur.tagName === 'BODY') {
+                    break
+                }
+                res.unshift(cloneNode(cur))
+                cur = cur.parentNode
+            }
+            return res
+        }
+
+        if (cnv.isArray(parentElelemns)) {
+            for (let i = 0; i < parentElelemns.length; i++) {
+                let parent = parentElelemns[i]
+                if (parent instanceof Element) {
+                    if (i === 0) {
+                        let n = cloneNodeUp(parent)
+                        nodes.push(...n)
+                    } else {
+                        nodes.push(cloneNode(parent))
+                    }
+                } else {
+                    let n = document.createElement('div')
+
+                    let classList = []
+                    if (cnv.isString(parent)) {
+                        classList.push(parent)
+                    } else if (cnv.isArray(parent)) {
+                        classList.push(...parent)
+                    }
+                    n.classList.add(...classList)
+                    nodes.push(cloneNode(n))
+                }
+            }
+        }
+
+
+        if (nodes.length === 0) {
+            // не было получено иерархии, делаем фиктивный корневой
+            nodes.push(document.createElement('div'))
+        }
+
+        // это последняя нода, источник размеров
+        nodes.push(document.createElement('div'))
+
+        let cur = nodes[0]
+        for (let i = 1; i < nodes.length; i++) {
+            let n = nodes[i]
+            cur.appendChild(n)
+            cur = n
+        }
+
+        return {
+            top: nodes[0],
+            el: nodes[nodes.length - 1],
+        }
+
+    }
+
+}
+
+//////
+
+/**
  * Инициализация модуля
  * @private
  */
---
+++ js-modules/jandcode-base/test/tests/dom.test.js
@@ -0,0 +1,33 @@
+import {tst, apx} from './vendor'
+
+import * as m from '../../src/dom'
+
+describe(module.id, function() {
+
+    it("1", function() {
+        let z = new m.PixelCalc({classList: ['sss']})
+        let a
+
+        a = z.getWidth("2em")
+        console.info("a", a);
+
+        a = z.getWidth(2.1)
+        console.info("a", a);
+
+        a = z.getHeight("3em")
+        console.info("a", a);
+
+        a = z.getHeight(3.1)
+        console.info("a", a);
+    })
+
+    it("NNchar", function() {
+        let z = new m.PixelCalc({classList: ['sss']})
+        let a
+
+        a = z.getWidth("10char")
+        console.info("a", a);
+    })
+
+})
+
