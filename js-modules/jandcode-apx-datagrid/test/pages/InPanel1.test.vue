6078020147c0fce45815b63aef562b69a827c3c9
gkraser <gkraser@gmail.com>
2021-12-23 12:49:58 +0600

    test

--- js-modules/jandcode-apx-datagrid/test/pages/InPanel1.test.vue
+++ js-modules/jandcode-apx-datagrid/test/pages/InPanel1.test.vue
@@ -1,8 +1,14 @@
 <template>
     <tst-panel>
 
-        <div class="row q-gutter-x-md q-mb-md" style="height:250px">
+        <template #tools>
+            <tst-checkbox label="Скрыть левую панель" v-model="hideLeftPanel"/>
+        </template>
 
+        <div class="row q-gutter-x-md q-mb-md" style="height:250px">
+            <jc-panel class="col" title="Это просто панель"
+                      :style="{display: hideLeftPanel?'none':'block'}">
+            </jc-panel>
             <jc-panel class="col" title="Это панель 1" body-fit no-padding>
                 <template #toolbar>
                     <jc-action icon="more-v"/>
@@ -41,5 +47,10 @@ export default {
     created() {
         this.opt1 = grid1({})
     },
+    data() {
+        return {
+            hideLeftPanel: false,
+        }
+    }
 }
 </script>
